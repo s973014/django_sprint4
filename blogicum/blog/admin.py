c8aa26d02bfd5437252896d46d53bb537f54f256
Stepan Demchenko <ese4@yandex.ru>
2025-02-17 20:56:35 +0700

    Blogicum v3

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,11 @@
+# from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Location, Post, Comment
+
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Post)
+admin.site.register(Comment)
